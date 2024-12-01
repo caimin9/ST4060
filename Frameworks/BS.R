a109f843045671c5d513dc02cc37303e7789c7bd
Caimin Ayres <110409985+caimin9@users.noreply.github.com>
2024-12-01 19:49:47 +0000

    Update BS.R

--- Frameworks/BS.R
+++ Frameworks/BS.R
@@ -2,4 +2,4 @@ B =1000
 n = length(x)
 
 #Can change mean for sd or whatever we want to compute
-replicate(B, mean(sample(x,size = n, replace =TRUE)
+replicate(B, mean(sample(x,size = n, replace =TRUE)))
